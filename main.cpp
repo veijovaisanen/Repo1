addfbb18f1a5591ca2bea1a6cf730e20b676bcbc
Veijo <vijvais@gmail.com>
2012-05-20 12:59:39 +0300

    Uusi

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <iomanip>
+//Comment here
+//A new line
+using namespace std;
+void main()
+{	 
+	int ika; 
+	char *etunimi; 
+	etunimi = new char[20];
+	char *sukunimi; 
+	sukunimi = new char[40];	
+	cout<<"\nAnna etunimi"<<endl;
+	cin>>etunimi;
+	cout<<"\nAnna sukunimi;"<<endl;
+	cin>>sukunimi;	
+	cout<<"\nAnna ikä;"<<endl;
+	cin>>ika;		
+	cout<<"\nNimi: "<<etunimi<<" "<<sukunimi<<","<<ika<<",vuotias"<<endl;
+	delete [] etunimi;
+	delete [] etunimi;
+}
